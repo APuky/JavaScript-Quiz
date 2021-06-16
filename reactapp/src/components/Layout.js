99921958d22dcf5aa0ee97d31282a204d9f69e8e
Stjepan Brnjak <stjepan136@gmail.com>
2021-06-16 15:09:41 +0200

    Login/SignUp/LogOut routes
    Routes are set and ready to be connected to the backend for user authentication

--- reactapp/src/components/Layout.js
+++ reactapp/src/components/Layout.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "../styles/Landing.module.scss";
+import Navbar from "./Navbar";
 
 function Layout(props) {
   return (
     <div className={styles.container}>
-      <div className={styles.relative_container}>{props.children}</div>
+      <div className={styles.relative_container}>
+        <Navbar />
+        <div> {props.children}</div>
+      </div>
     </div>
   );
 }
