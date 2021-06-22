b518cb4f2a37a507b8ac941c66fc0f863e89512b
Stjepan Brnjak <stjepan136@gmail.com>
2021-06-22 19:29:15 +0200

    routes design update

--- reactapp/src/components/Main.js
+++ reactapp/src/components/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/Landing.module.scss";
 import { useHistory } from "react-router-dom";
+import Illustration from "./illustrations/IllustrationMain";
 
 function Main() {
   const [isAuth, setIsAuth] = useState(false);
@@ -21,19 +22,20 @@ function Main() {
   };
 
   return (
-    <>
-      <div className={styles.title}>
-        <h4>Welcome "Implement logic"</h4>
-        How well do you know <br /> <span>J</span>ava<span>S</span>cript?
-        <h4>Test your knowledge with the challenge</h4>
+    <div className={styles.landing_page}>
+      <div className={styles.intro}>
+        {" "}
+        <div className={styles.title}>
+          <h4>Welcome "Implement logic"</h4>
+          How well do you know <br /> <span>J</span>ava<span>S</span>cript?
+        </div>
+        <button className={styles.btn} onClick={() => toTestHandler()}>
+          Test Now
+        </button>
       </div>
-      <button
-        style={{ marginLeft: "7rem", fontSize: "1.9rem" }}
-        onClick={() => toTestHandler()}
-      >
-        Test Now
-      </button>
-    </>
+
+      <Illustration />
+    </div>
   );
 }
 
