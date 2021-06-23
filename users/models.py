aae0e0ae18323016af789a4510b115b8d014e5d1
APuky <a.pukanic@gmail.com>
2021-06-23 16:51:56 +0200

    Backend API for scoreboard and accounts

--- users/models.py
+++ users/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    score = models.IntegerField(default = 0)
     def __str__(self):
         return self.email
