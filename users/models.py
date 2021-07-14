f217488d20e99b20a94c65484df09ba8d3f4a1ca
APuky <a.pukanic@gmail.com>
2021-07-14 14:58:05 +0200

    Possibly figured it out, user should now get data when clicking on account tab

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    score = models.IntegerField(default = 0)
+    score = models.IntegerField(default = None, null = True)
     quizTaken = models.IntegerField(default = 0)
     def __str__(self):
         return self.email
