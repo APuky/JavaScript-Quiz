3b3e68dfd695a7a764edbb578885d85bf6c9066e
Subham <subhamchauhan982@gmail.com>
2022-06-21 13:14:20 +0530

    test commit

--- reactapp/src/components/Questions.js
+++ reactapp/src/components/Questions.js
@@ -1,104 +1,104 @@
-import React, { useState, useEffect } from "react";
-import { questions } from "./QuestionsData";
-import styles from "../styles/Questions.module.scss";
-import SelectButton from "./SelectButton";
-import { motion } from "framer-motion";
-import { pageAnimation } from "./Animation";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { questions } from './QuestionsData'
+import styles from '../styles/Questions.module.scss'
+import SelectButton from './SelectButton'
+import { motion } from 'framer-motion'
+import { pageAnimation } from './Animation'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 function Questions() {
-  let history = useHistory();
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState("");
-  const [correctAnswerNumber, setCorrectAnswerNumber] = useState("");
-  const [selectedAnswerBoolean, setSelectedAnswerBoolean] = useState("");
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [isAnswerSelected, setIsAnswerSelected] = useState(styles.btn_opacity);
-  const [correctAnswers, setCorrectAnswers] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const [minutes, setMinutes] = useState(2);
+  let history = useHistory()
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState('')
+  const [correctAnswerNumber, setCorrectAnswerNumber] = useState('')
+  const [selectedAnswerBoolean, setSelectedAnswerBoolean] = useState('')
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [isAnswerSelected, setIsAnswerSelected] = useState(styles.btn_opacity)
+  const [correctAnswers, setCorrectAnswers] = useState(0)
+  const [timeLeft, setTimeLeft] = useState(0)
+  const [minutes, setMinutes] = useState(2)
 
   useEffect(() => {
     questions[currentQuestion].answersAll.find((ans, index) => {
-      return ans.isCorrect === true ? setCorrectAnswerNumber(index + 1) : null;
-    });
-  }, [currentQuestion]);
+      return ans.isCorrect === true ? setCorrectAnswerNumber(index + 1) : null
+    })
+  }, [currentQuestion])
 
   useEffect(() => {
-    if (selectedAnswerBoolean === "") {
-      return;
-    } else setIsAnswerSelected(styles.btn);
-  }, [selectedAnswerBoolean]);
+    if (selectedAnswerBoolean === '') {
+      return
+    } else setIsAnswerSelected(styles.btn)
+  }, [selectedAnswerBoolean])
 
   useEffect(() => {
     if (minutes === 0 && timeLeft === 0) {
-      setSelectedAnswerBoolean(false);
-      setIsAnswerCorrect(false);
-      submitHandler();
+      setSelectedAnswerBoolean(false)
+      setIsAnswerCorrect(false)
+      submitHandler()
     }
 
     if (timeLeft === 0 && minutes) {
-      setMinutes(minutes - 1);
-      setTimeLeft(59);
-    } else if (!timeLeft) return;
+      setMinutes(minutes - 1)
+      setTimeLeft(59)
+    } else if (!timeLeft) return
 
     const intervalId = setInterval(() => {
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
+      setTimeLeft(timeLeft - 1)
+    }, 1000)
 
-    return () => clearInterval(intervalId);
+    return () => clearInterval(intervalId)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timeLeft]);
+  }, [timeLeft])
 
   const answerHandler = (answer) => {
     answer === true
       ? setSelectedAnswerBoolean(true)
-      : setSelectedAnswerBoolean(false);
-  };
+      : setSelectedAnswerBoolean(false)
+  }
 
   const submitHandler = () => {
-    if (selectedAnswerBoolean === "") {
-      return;
+    if (selectedAnswerBoolean === '') {
+      return
     } else if (selectedAnswerBoolean === true) {
-      setCorrectAnswers(correctAnswers + 1);
-      setIsAnswerCorrect(true);
+      setCorrectAnswers(correctAnswers + 1)
+      setIsAnswerCorrect(true)
     } else {
-      setIsAnswerCorrect(false);
+      setIsAnswerCorrect(false)
     }
-  };
+  }
 
   const nextQuestionHandler = () => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token')
 
     if (currentQuestion >= questions.length - 1) {
-      localStorage.setItem("score", correctAnswers);
+      localStorage.setItem('score', correctAnswers)
       const data = {
         score: correctAnswers,
         quizTaken: 1,
-      };
+      }
       const promise = axios.patch(
-        "http://127.0.0.1:8000/api/users/auth/user/",
+        'http://127.0.0.1:8000/api/users/auth/user/',
         data,
         {
           headers: { Authorization: `Token ${token}` },
-        }
-      );
-      history.push("/account");
-      return promise;
+        },
+      )
+      history.push('/account')
+      return promise
     } else {
-      setCurrentQuestion(currentQuestion + 1);
-      setIsAnswerCorrect("");
-      setCorrectAnswerNumber("");
-      setSelectedAnswerBoolean("");
+      setCurrentQuestion(currentQuestion + 1)
+      setIsAnswerCorrect('')
+      setCorrectAnswerNumber('')
+      setSelectedAnswerBoolean('')
     }
 
-    setTimeLeft(59);
-    setMinutes(1);
-  };
+    setTimeLeft(59)
+    setMinutes(1)
+  }
 
   // eslint-disable-next-line eqeqeq
-  if (localStorage["quizTaken"] == 1) {
-    return <h2>Test already taken</h2>;
+  if (localStorage['quizTaken'] == 1) {
+    return <h2>Test already taken</h2>
   } else {
     return (
       <div className={styles.container}>
@@ -140,7 +140,7 @@ function Questions() {
           </div>
         )}
         {isAnswerCorrect === true && <h3>Correct!</h3>}
-        {isAnswerCorrect === "" ? (
+        {isAnswerCorrect === '' ? (
           <div className={styles.submitting}>
             <button
               className={isAnswerSelected}
@@ -158,8 +158,8 @@ function Questions() {
           </button>
         )}
       </div>
-    );
+    )
   }
 }
 
-export default Questions;
+export default Questions
